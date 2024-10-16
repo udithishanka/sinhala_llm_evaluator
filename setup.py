bbf662568abc82b84c19a614ffefcfc3ab515a04
Udith Ishanka <107479890+udithishanka@users.noreply.github.com>
2024-10-16 16:30:11 +0530

    Add build and upload commands to bash script

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sinhala_llm_evaluator",  # Your package name
+    version="0.1.0",  # Initial version
+    description="A module for evaluating language models during training",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/udithishanka",  # Your repo or homepage
+    author="Udith Ishanka",
+    author_email="udithishanka.s@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "transformers",
+        "datasets",
+        "evaluate",
+        "numpy",
+        "nltk",
+        "absl-py",
+        "rouge-score",
+        "scikit-learn"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
